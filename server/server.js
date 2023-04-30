6f6910b55f3c62934baa1e2150d17bd54d267fd0
Anmew Punyakon <Apissara.aun@gmail.com>
2023-04-30 22:38:02 +0700

    Update

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(process.env.APP_NAME);
+});
+app.get("/test", (req, res) => {
+  res.send("test");
+});
+
+app.listen(3000);
